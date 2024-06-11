bb66598c81be76a2b82a5436180c25c88ef1010f
Disrupi <95573431+Disrupi@users.noreply.github.com>
2024-06-11 18:55:49 +0200

    Create OSDCloud_Prod.ps1

---
+++ OSDCloud_Prod.ps1
@@ -0,0 +1,30 @@
+Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
+
+
+Install-Module OSD -Force -Skippublishercheck | out-null
+Install-Module MSCatalog -Force -Skippublishercheck | out-null
+
+
+#$Global:MyOSDCloud = @{
+    #DriverPackName = 'none'
+    #ApplyManufacturerDrivers = $false
+    #ApplyCatalogDrivers = $true
+    #ApplyCatalogFirmware = $true
+    #MSCatalogDiskDrivers = $true
+    #MSCatalogNetDrivers = $true
+    #MSCatalogScsiDrivers = $true
+    #MSCatalogFirmware = $true
+#}
+
+#
+
+$Params = @{
+    OSVersion = "Windows 11"
+    OSBuild = "22H2"
+    OSEdition = "Enterprise"
+    OSLanguage = "en-us"
+    ZTI = $true
+    Firmware = $false
+}
+Start-OSDCloud @Params
+wpeutil reboot
