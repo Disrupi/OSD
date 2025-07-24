c3d772e8bc72177375282283073b14135edf88f6
Disrupi <95573431+Disrupi@users.noreply.github.com>
2025-07-24 18:09:54 +0200

    Create Testing.ps1

---
+++ Testing.ps1
@@ -0,0 +1,56 @@
+#New-OSDCloudUSB -fromIsoFile C:\ProgramData\OSDCloud\Templates\prod_v3\OSDCloud_NoPrompt.iso
+Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
+
+
+Install-Module OSD -Force -Skippublishercheck | out-null
+Install-Module MSCatalog -Force -Skippublishercheck | out-null
+#########
+#########
+
+#$Global:MyOSDCloud = @{
+   # DriverPackName = 'Microsoft Update Catalog'
+   # ApplyManufacturerDrivers = $false
+   # ApplyCatalogDrivers = $true
+   # ApplyCatalogFirmware = $true
+   # MSCatalogDiskDrivers = $true
+   # MSCatalogNetDrivers = $true
+   # MSCatalogScsiDrivers = $true
+   # MSCatalogFirmware = $true
+#}
+
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$True
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$true
+    WindowsUpdateDrivers = [bool]$true
+    WindowsDefenderUpdate = [bool]$true
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$False
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$true
+    updateFirmware = [bool]$true
+    CheckSHA1 = [bool]$true
+}
+
+#for all devices
+$Global:MyOSDCloud.DriverPackName = 'Microsoft Update Catalog'
+
+#for lenovo
+#$Global:MyOSDCloud.DriverPackName = 'Lenovo'
+
+
+
+$Global:MyOSDCloud
+
+$Params = @{
+    OSVersion = "Windows 11"
+    OSBuild = "24H2"
+    OSEdition = "Enterprise"
+    OSLanguage = "en-us"
+    #ZTI = $true
+    #Firmware = $false
+}
+Start-OSDCloud @Params
+#wpeutil reboot
+#start-osdcloudgui
